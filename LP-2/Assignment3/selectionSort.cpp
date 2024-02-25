7570da6ef6af9fa519858e6453aeaa80179853f7
Bhushan Thombare <125019953+thombarebhushan@users.noreply.github.com>
2024-02-25 23:32:23 +0530

    Update selectionSort.cpp

--- LP-2/Assignment3/selectionSort.cpp
+++ LP-2/Assignment3/selectionSort.cpp
@@ -5,4 +5,4 @@ III. Single-Source Shortest Path Problem
 IV. Job Scheduling Problem 
 V. Prim's Minimal Spanning Tree Algorithm 
 VI. Kruskal's Minimal Spanning Tree Algorithm 
-VII. Dijkstra's Minimal Spanning Tree Algorithm*/
+VII. Dijkstra's Minimal Spanning  Tree Algorithm*/
